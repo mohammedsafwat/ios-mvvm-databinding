733e419aa888d7db26c522fa3354a33bc0889fbf
Mohammed Safwat <m.safwat@holidaypirates.com>
2018-06-05 18:06:31 +0200

    Add a mock class for UserLocalDataSource.

---
+++ GitHubUsersTests/Data/DataSourcesTests/MockClasses/MockUserLocalDataSource.swift
@@ -0,0 +1,42 @@
+//
+//  MockUserLocalDataSource.swift
+//  GitHubUsersTests
+//
+//  Created by Mohammed Safwat on 6/4/18.
+//  Copyright © 2018 Mohammed Safwat. All rights reserved.
+//
+
+import Foundation
+@testable import GitHubUsers
+
+class MockUserLocalDataSource: UserLocalDataSource {
+    var isDataAvailable: Bool = false
+    var mockUserData: UserData?
+    var fetchUserDataIsCalled: Bool = false
+    var saveUserDataIsCalled: Bool = false
+    var deleteUserDataIsCalled: Bool = false
+    var refreshUserDataIsCalled: Bool = false
+    
+    override func fetchUserData(username: String, onSuccess: SuccessCallBack?, onError: ErrorCallBack?) {
+        fetchUserDataIsCalled = true
+        
+        if isDataAvailable {
+            onSuccess?(mockUserData!)
+        } else {
+            let error = NSError(domain: "com.safwat.GitHubUsers", code: 401, userInfo: nil)
+            onError?(error)
+        }
+    }
+    
+    override func saveUserData(userData: UserData, onError: ErrorCallBack?) {
+        saveUserDataIsCalled = true
+    }
+    
+    override func deleteUserData(userId: Int, onError: ErrorCallBack?) {
+        deleteUserDataIsCalled = true
+    }
+    
+    override func refreshUserData() {
+        refreshUserDataIsCalled = true
+    }
+}
